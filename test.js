795828a1be085dc3f66f0f372136cfd17209b2af
akiroz <akiroz.vectis@gmail.com>
2017-03-16 17:42:11 +0800

    add test

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 const skygear = require('skygear');
-const {getMac} = require('getmac');
 
 const {
   endPoint,
@@ -9,7 +8,7 @@ const {
 } = require('./config.json');
 
 async function main() {
-  console.log('### Skygear IoT Client ###');
+  console.log('### Skygear IoT Ping Test ###');
   console.log(`Skygear Endpoint: ${endPoint}`);
   console.log(`Skygear User: ${username}`);
   process.stdout.write('Initializing... ');
@@ -17,13 +16,13 @@ async function main() {
   await skygear.loginWithUsername(username, password);
   console.log('Done!');
 
-  // respond to 'ping' events with 'pong' event and device's MAC address
-  skygear.on('ping', _ => {
-    getMac((err, macAddress) => {
-      if(err) throw err;
-      skygear.pubsub.publish('pong', macAddress);
-    });
+  // listen for 'pong' events
+  skygear.on('pong', mac => {
+    console.log(`[pong] ${mac}`);
   });
+
+  console.log('Sending ping...')
+  skygear.pubsub.publish('ping', {});
 
 }
 
